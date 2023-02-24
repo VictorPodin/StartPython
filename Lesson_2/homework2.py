d683267cc621315d47599fb2bbd79fbd5da54200
Victor Podin <podin_victor@mail.ru>
2023-02-24 16:55:52 +0300

    seminar 2 homework done

--- Lesson_2/homework2.py
+++ Lesson_2/homework2.py
@@ -7,14 +7,14 @@
 # 5 6 -> 2 3
 sum = int(input('Введите сумму загаданных чисел:'))
 mul = int(input('Введите произведение загаданных чисел:'))
-x=1
+x = 1
 for i in range(1000):
-    if x*x - sum*x + mul==0:
+    if x*x - sum*x + mul == 0:
         break
     else:
-        x+=1
-if x==1001:
+        x += 1
+if x == 1001:
     print('Ошибся ты, Петя, не можем найти числа с такими суммой и произведением')
 else:
-    y=sum-x
-    print(x,y)
+    y = sum-x
+    print(x, y)
