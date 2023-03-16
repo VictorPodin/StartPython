3ccc0bea1a4b98b1bbfae2d071ab5eb93c1d4bd9
Victor Podin <podin_victor@mail.ru>
2023-03-16 23:15:02 +0300

    seminar 8 homework

---
+++ Lesson_8/task1.49.py
@@ -0,0 +1,123 @@
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. 
+# Пользователь также может ввести имя или фамилию, 
+# и Вы должны реализовать функционал для изменения и удаления данных
+
+from os import path
+
+file_base = "base.txt"
+last_id = 0
+all_data = []
+
+if not path.exists(file_base):
+    with open(file_base, "w", encoding="utf-8") as _:
+        pass
+
+def read_records():
+    global last_id, all_data
+
+    with open(file_base, encoding="utf-8") as f:
+        all_data = [i.strip() for i in f]
+        if all_data:
+            last_id = int(all_data[-1].split()[0])
+            return all_data
+    return []
+
+def show_all():
+    if all_data:
+        print(*all_data, sep="\n")
+    else: 
+        print("Empty Data\n")
+
+def add_new_contact():
+    global last_id
+    array = ["surname", "name", "patronym", "phone_number"]    #template
+    string = ""
+    for i in array:
+        string += input(f"Enter {i} ") + " "
+    last_id += 1
+
+    with open(file_base, "a", encoding="utf-8") as f:
+        f.write(f"{last_id} {string}\n")
+
+def search_contact():
+    searched = input("Введите что-то для поиска по справочнику:")
+    searched = searched.split()
+    found = False
+    for i in searched:
+        for j in all_data:
+            if i in j: 
+                print("Search Result: ", j)
+                found = True
+    if found:
+        print("That's all we found\n")
+    else: 
+        print("Not found, try again? press 3")
+
+def change_contact():
+    modID = int(input("Введите ID контакта для редактирования:"))
+    global last_id
+    if modID <= last_id:
+        print()
+        answer = input("Что хотите изменить:\n"
+                       "1. Фамилия\n"
+                       "2. Имя\n"
+                       "3. Отчество\n"
+                       "4. Телефон\n"
+                       "5. Exit\n")
+        match answer:
+            case "1":
+                newinput("Введите новую фамилию:\n")
+            case "2":
+                input("Введите новое имя:\n")
+            case "3":
+                input("Введите новое отчество:\n")
+            case "4":
+                input("Введите новый телефон:\n")
+            case "5":
+                return
+            case _:
+                print("Try again!\n") 
+
+
+    array = ["surname", "name", "patronym", "phone_number"]    #template
+    string = ""
+    for i in array:
+        string += input(f"Enter {i} ") + " "
+    last_id += 1
+
+    with open(file_base, "a", encoding="utf-8") as f:
+        f.write(f"{last_id} {string}\n")
+
+
+def main_menu():
+    play = True
+    while play:
+        read_records()
+        answer = input("Phone book menu:\n"
+                       "1. Show all records\n"
+                       "2. Add a record\n"
+                       "3. Search a record\n"
+                       "4. Changea record\n"
+                       "5. Delete\n"
+                       "6. Export/Import\n"
+                       "7. Exit\n")
+        match answer:
+            case "1":
+                show_all()
+            case "2":
+                add_new_contact()
+            case "3":
+                search_contact()
+            case "4":
+                change_contact()
+            case "5":
+                pass
+            case "6":
+                pass
+            case "7":
+                play = False
+            case _:
+                print("Try again!\n")
+
+main_menu()
+            
