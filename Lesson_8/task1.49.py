91230be0ec3de8ea2a66c1cecce7cbe25c61f796
Victor Podin <podin_victor@mail.ru>
2023-03-21 00:15:06 +0300

    seminar 8 homework

--- Lesson_8/task1.49.py
+++ Lesson_8/task1.49.py
@@ -28,16 +28,25 @@ def show_all():
     else: 
         print("Empty Data\n")
 
+def iSexist(str):
+    for i in all_data:
+        if i == str:
+            return True
+    return False
+    
 def add_new_contact():
     global last_id
     array = ["surname", "name", "patronym", "phone_number"]    #template
     string = ""
     for i in array:
         string += input(f"Enter {i} ") + " "
-    last_id += 1
+    if iSexist(string) == False:
+        last_id += 1
 
-    with open(file_base, "a", encoding="utf-8") as f:
-        f.write(f"{last_id} {string}\n")
+        with open(file_base, "a", encoding="utf-8") as f:
+            f.write(f"{last_id} {string}\n")
+    else:
+        print("Already in Base")
 
 def search_contact():
     searched = input("Введите что-то для поиска по справочнику:")
@@ -54,41 +63,61 @@ def search_contact():
         print("Not found, try again? press 3")
 
 def change_contact():
+    show_all()
     modID = int(input("Введите ID контакта для редактирования:"))
-    global last_id
+    global last_id, all_data
     if modID <= last_id:
-        print()
-        answer = input("Что хотите изменить:\n"
+        with open(file_base, 'w', encoding="utf-8") as f:
+            f.write("")
+        for i in all_data:
+            j = int(i.split()[0])
+            if j == modID:
+                changes = i.split()
+                answer = input("Что хотите изменить:\n"
                        "1. Фамилия\n"
                        "2. Имя\n"
                        "3. Отчество\n"
                        "4. Телефон\n"
                        "5. Exit\n")
-        match answer:
-            case "1":
-                newinput("Введите новую фамилию:\n")
-            case "2":
-                input("Введите новое имя:\n")
-            case "3":
-                input("Введите новое отчество:\n")
-            case "4":
-                input("Введите новый телефон:\n")
-            case "5":
-                return
-            case _:
-                print("Try again!\n") 
+                match answer:
+                    case "1":
+                        changes[1] = input("Введите новую фамилию:\n")
+                    case "2":
+                        changes[2] = input("Введите новое имя:\n")
+                    case "3":
+                        changes[3] = input("Введите новое отчество:\n")
+                    case "4":
+                        changes[4] = input("Введите новый телефон:\n")
+                    case "5":
+                        return
+                    case _:
+                        print("Try again!\n") 
+    changes = " ".join(changes)
+ ####
+    for i in all_data:
+            j = int(i.split()[0])
+            if j == modID:
+                with open(file_base, "a", encoding="utf-8") as f:
+                    f.write(f"{changes} \n")
+            else:
+                with open(file_base, "a", encoding="utf-8") as f:
+                    f.write(f"{i} \n")
 
+def del_contact():
+    global all_data
 
-    array = ["surname", "name", "patronym", "phone_number"]    #template
-    string = ""
-    for i in array:
-        string += input(f"Enter {i} ") + " "
-    last_id += 1
-
-    with open(file_base, "a", encoding="utf-8") as f:
-        f.write(f"{last_id} {string}\n")
-
-
+    symbol = "\n" ### вставить в конструкцию ниже нельзя без переменной
+    show_all()
+    delId = int(input("Введите id записи для удаления: "))
+    with open(file_base, 'w', encoding="utf-8") as f:
+        f.write("")
+    for i in all_data:
+            j = int(i.split()[0])
+            with open(file_base, 'a', encoding="utf-8") as f:
+                if j != delId:
+                    f.write(f'{i}\n')
+    print("Record deleted!\n")
+    
 def main_menu():
     play = True
     while play:
@@ -97,7 +126,7 @@ def main_menu():
                        "1. Show all records\n"
                        "2. Add a record\n"
                        "3. Search a record\n"
-                       "4. Changea record\n"
+                       "4. Change a record\n"
                        "5. Delete\n"
                        "6. Export/Import\n"
                        "7. Exit\n")
@@ -111,7 +140,7 @@ def main_menu():
             case "4":
                 change_contact()
             case "5":
-                pass
+                del_contact()
             case "6":
                 pass
             case "7":
